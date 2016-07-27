86db73a36a647c43e99d65c4285944c637b45700
Connor Dougherty <connordougherty01@gmail.com>
2016-07-27 11:25:52 -0400

    Added Storefront

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160726142100) do
+ActiveRecord::Schema.define(version: 20160727140903) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -26,9 +26,13 @@ ActiveRecord::Schema.define(version: 20160726142100) do
     t.text     "description"
     t.integer  "rating"
     t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "image"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.string   "brand"
   end
 
 end

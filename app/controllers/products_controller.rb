c0859f6e44b8a70d483b3a193b92c0328443bdb0
Connor Dougherty <connordougherty01@gmail.com>
2016-07-28 11:08:50 -0400

    sort by, category and brand

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,12 +15,10 @@ class ProductsController < ApplicationController
   # GET /products/new
   def new
     @product = Product.new
-    @categories = Category.all
   end
 
   # GET /products/1/edit
   def edit
-    @categories = Category.all
   end
 
   # POST /products
